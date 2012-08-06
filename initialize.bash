8e45100e3b2927c02e012e2e09c670824956f62b
Te[ss]a <thanh.x.pham@me.com>
2012-08-06 18:15:35 +0200

    * ln doesn't has -h here

--- initialize.bash
+++ initialize.bash
@@ -7,10 +7,10 @@ here="$(cd "$here"; pwd)"
 for file in "$here"/*; do
     name="$(basename "$file")"
     if [[ !( " initialize.bash oh-my-zsh-custom readme.md " =~ " $name " || -d $file/.git ) ]]; then
-        ln -sfhv $file "$HOME/.$name"
+        ln -sfv $file "$HOME/.$name"
     fi
 done
 
-find "$here/oh-my-zsh-custom/custom" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/.oh-my-zsh/custom/"
-find "$here/oh-my-zsh-custom/themes" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/.oh-my-zsh/themes/"
+find "$here/oh-my-zsh-custom/custom" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/.oh-my-zsh/custom/"
+find "$here/oh-my-zsh-custom/themes" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/.oh-my-zsh/themes/"
 
